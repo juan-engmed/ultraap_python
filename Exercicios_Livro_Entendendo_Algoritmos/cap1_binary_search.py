e510301933e3fc5121a4d7ff7cf022a16551cebc
Juan Alien <alien_juandev@outlook.com>
2025-03-25 07:53:48 -0300

    upload binary_search

---
+++ Exercicios_Livro_Entendendo_Algoritmos/cap1_binary_search.py
@@ -0,0 +1,29 @@
+
+def search_element_by_binary_search(array, value):
+    first_index = 0
+    last_index = len(array) - 1
+
+    while first_index <= last_index:
+        middle_index = (last_index + first_index) // 2
+        guess_value = array[middle_index]
+
+        if guess_value == value:
+            return middle_index
+        elif guess_value > value:
+            last_index = middle_index - 1
+        else:
+            first_index  = middle_index + 1
+    return None        
+
+def main():
+    test_list = [1,2,3,4,5]
+    target = 11
+
+    target_index = search_element_by_binary_search(test_list, target)
+    if target_index is not None:
+        print(f'O índice do elemento {target} é: {target_index}')
+    else:
+        print(f'O elemento {target} não foi encontrado na lista.')
+
+if __name__ == "__main__":
+    main()
